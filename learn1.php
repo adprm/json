ed23a7227f6a20d08c599e3d3e644412d94b6c86
adprm <aditiyaprmn00@gmail.com>
2020-08-15 03:08:28 +0700

    connect to db

--- learn1.php
+++ learn1.php
@@ -1,17 +1,21 @@
 <?php
 
-$mahasiswa = [
-    [
-        "nama" => "Aditiya Permana",
-        "npm" => "12345678",
-        "email" => "aditiyaprmn00@gmail.com"
-    ],
-    [
-        "nama" => "Maulana",
-        "npm" => "876543321",
-        "email" => "maulana@gmail.com"
-    ]
-];
+// $mahasiswa = [
+//     [
+//         "nama" => "Aditiya Permana",
+//         "npm" => "12345678",
+//         "email" => "aditiyaprmn00@gmail.com"
+//     ],
+//     [
+//         "nama" => "Maulana",
+//         "npm" => "876543321",
+//         "email" => "maulana@gmail.com"
+//     ]
+// ];
+
+$dbh = new PDO('mysql:host=localhost;dbname=crud-ci3', 'root');
+
+$db = $dbh->prepare('SELECT * FROM student');
 
 $data = json_encode($mahasiswa);
 echo $data;
