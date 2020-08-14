906ff0f101d2e55bf564afe7287c6e81aebdfa46
adprm <aditiyaprmn00@gmail.com>
2020-08-15 03:46:41 +0700

    json parse

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // mengubah objek menjadi json
-// let mahasiswa = {
-//     nama: "Aditiya Permana",
-//     nim: "151098654",
-//     email: 'adit@mail.com'
-// };
+let mahasiswa = {
+    nama: "Aditiya Permana",
+    nim: "151098654",
+    email: 'adit@mail.com'
+};
 
-// console.log(JSON.stringify(mahasiswa));
+console.log(JSON.stringify(mahasiswa));
 
 // json ke objek
 let xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
-        let mahasiswa = this.responseText;
+        // json ke objek
+        let mahasiswa = JSON.parse(this.responseText);
         console.log(mahasiswa);
     }
 }
